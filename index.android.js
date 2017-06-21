09b5fbe7e853f47dba211b4a3b0bdeef110198f8
Joever Monceda <joever.monceda@gmail.com>
2017-06-21 13:40:07 +0800

    first commit

---
+++ index.android.js
@@ -0,0 +1,58 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+ import MapView from 'react-native-maps';
+
+export default class mapV2 extends Component {
+  render() {
+    return (
+      <MapView
+           style={styles.map}
+           initialRegion={{
+             latitude: 37.78825,
+             longitude: -122.4324,
+             latitudeDelta: 0.0922,
+             longitudeDelta: 0.0421,
+           }}
+         />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  }
+});
+
+AppRegistry.registerComponent('mapV2', () => mapV2);
